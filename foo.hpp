0984e263a266534211a97679f7c62b076c7ddd9f
jestemboski999 <jestemboski999@gmail.com>
2022-12-13 11:29:40 +0100

    v1

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,23 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector<char> v1;
+    int size = v1.capacity();
+    std::list<Human>::iterator it;
+    
+    for (it = people.end(); it != people.begin(); it--)
+    {
+    it->birthday();
+    
+    if(it->isMonster())
+    {
+        v1.push_back('n');
+    }
+    else
+    {
+        v1.push_back('y');
+    }
+    }
+
+return(v1);
 }
